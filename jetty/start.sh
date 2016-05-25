fc6986f942b5afaf94a0734c6792d8c5931d47cc
leo <cwq0312@163.com>
2016-05-25 17:15:06 +0800

    add

--- jetty/start.sh
+++ jetty/start.sh
@@ -3,4 +3,4 @@
 
 ##java -jar start.jar etc/jetty.xml
 #rm -rf nohup.out
-nohup java -XX:+HeapDumpOnOutOfMemoryError -Xms1024m -Xmx1024m -Xmn512m -XX:PermSize=128m -XX:MaxPermSize=128m  -DSTOP.PORT=8009 -DSTOP.KEY=qudream  -jar start.jar  jetty.port=8080  &
+nohup java -XX:+HeapDumpOnOutOfMemoryError -Xms256m -Xmx256m -Xmn128m -XX:PermSize=96m -XX:MaxPermSize=96m  -DSTOP.PORT=8009 -DSTOP.KEY=qudream  -jar start.jar  jetty.port=8080  &
